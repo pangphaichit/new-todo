b3609b737e3277859433cc4c38ecf6fe8083e0d1
pangphaichit <pg_grj@hotmail.com>
2026-02-24 17:27:02 +0700

    feat: persist todos using AsyncStorage

--- store/store.ts
+++ store/store.ts
@@ -1,5 +1,7 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { v4 as uuidv4 } from "uuid";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface Todo {
   id: string;
@@ -21,37 +23,45 @@ interface TodoState {
   deepTasks: () => Todo[];
   easyTasks: () => Todo[];
 }
-export const useTodoStore = create<TodoState>((set, get) => ({
-  todos: [],
-  addTodo: (todo) =>
-    set((state) => ({
-      todos: [
-        ...state.todos,
-        {
-          ...todo,
-          id: uuidv4(),
-          done: false,
-        },
-      ],
-    })),
-  updateTodo: (id, data) =>
-    set((state) => ({
-      todos: state.todos.map((t) => (t.id === id ? { ...t, ...data } : t)),
-    })),
+export const useTodoStore = create<TodoState>()(
+  persist(
+    (set, get) => ({
+      todos: [],
+      addTodo: (todo) =>
+        set((state) => ({
+          todos: [
+            ...state.todos,
+            {
+              ...todo,
+              id: uuidv4(),
+              done: false,
+            },
+          ],
+        })),
+      updateTodo: (id, data) =>
+        set((state) => ({
+          todos: state.todos.map((t) => (t.id === id ? { ...t, ...data } : t)),
+        })),
 
-  deleteTodo: (id: string) =>
-    set((state) => ({
-      todos: state.todos.filter((t) => t.id !== id),
-    })),
+      deleteTodo: (id: string) =>
+        set((state) => ({
+          todos: state.todos.filter((t) => t.id !== id),
+        })),
 
-  toggleTodo: (id: string) =>
-    set((state) => ({
-      todos: state.todos.map((t) =>
-        t.id === id ? { ...t, done: !t.done } : t,
-      ),
-    })),
+      toggleTodo: (id: string) =>
+        set((state) => ({
+          todos: state.todos.map((t) =>
+            t.id === id ? { ...t, done: !t.done } : t,
+          ),
+        })),
 
-  deepTasks: () => get().todos.filter((t) => t.category === "deep"),
+      deepTasks: () => get().todos.filter((t) => t.category === "deep"),
 
-  easyTasks: () => get().todos.filter((t) => t.category === "easy"),
-}));
+      easyTasks: () => get().todos.filter((t) => t.category === "easy"),
+    }),
+    {
+      name: "todo-storage",
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
